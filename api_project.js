3093d17fa06eb9d94d51d5e040f23b456d17c841
root <root@admin.(none)>
2015-09-15 19:11:10 -0700

    test

---
+++ api_project.js
@@ -0,0 +1,15 @@
+define({
+  "name": "vita-docs",
+  "version": "0.1.0",
+  "description": "vita api docs",
+  "title": "vita api docs",
+  "url": "http://54.219.139.212/vita-api/",
+  "sampleUrl": false,
+  "apidoc": "0.2.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2015-09-16T01:22:02.660Z",
+    "url": "http://apidocjs.com",
+    "version": "0.13.1"
+  }
+});
